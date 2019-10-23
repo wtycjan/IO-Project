8b31cda603220ee98942f50f4e7ff53eabb8359f
Grzegorz Zelek <50342912+gzelek8@users.noreply.github.com>
2019-10-24 01:20:25 +0200

    Add files via upload
    Zależności między funkcjami

--- functions.py
+++ functions.py
@@ -33,13 +33,13 @@ def how_many_lines(file):
 
 #funkcja zlicza ilość różnych linii, jako wartość przyjmuje ścieżke do pliku, zwraca serie
 def list_of_lines(file):
-    list_of_lines = []
+    list_of_lines1 = []
     with open(file, 'r') as f:
         for linia in f:
-            list_of_lines.append(linia)
+            list_of_lines1.append(linia)
 
-    list_of_lines = pd.Series(list_of_lines)
-    unique_values = list_of_lines.value_counts()
+    list_of_lines1 = pd.Series(list_of_lines1)
+    unique_values = list_of_lines1.value_counts()
 
     return(unique_values)
 
@@ -90,4 +90,55 @@ def file_connect_weight(file):
         print(m)
         print("Lack of file")
 
+def function_connect(file):
+    try:
+        functions_list = []
+        for first in file:
+            with open(first, 'r') as f:
+                for line in f:
+                    poloczenia = line.split()
+                    if poloczenia and poloczenia[0] == "def":
+                        pom = poloczenia[1].split('(')
+                        functions_list.append(pom[0])
 
+        return functions_list
+
+    except IndexError as m:
+        print(m)
+        print("Lack of file")
+
+def function_connect_weight(file, function_list):
+    try:
+        how_many_function = []
+        for cos in function_list:
+            for first in file:
+                with open(first, 'r') as f:
+                    for line in f:
+                        polaczenia = line.split()
+                        if polaczenia and polaczenia[0] != 'import':
+                            for el in polaczenia:
+                                polaczenia_dalej = el.split('.')
+                                for el_dalej in polaczenia_dalej:
+                                    polaczenia_dalej1 = el_dalej.split('(')
+                                    if cos == polaczenia_dalej1[0]:
+                                        how_many_function.append(polaczenia_dalej1[0])
+        return how_many_function
+
+
+    except IndexError as m:
+        print(m)
+        print("Lack of file")
+
+def write_to_file_fun_data(list1,list2):
+    fun_info = []
+    fi = open('function_weight.txt', 'a')
+    for el in list1:
+        licznik = -1
+        for element in list2:
+            if el == element:
+                licznik += 1
+        fi.write(el)
+        fi.write("\n")
+        fi.write(str(licznik))
+        fi.write("\n")
+    fi.close()
--- main.py
+++ main.py
@@ -8,6 +8,9 @@ functions.how_many_lines(result)
 functions.file_connect(result)
 
 functions.file_connect_weight(result)
+function_list1 = functions.function_connect(result)
+function_list2 = functions.function_connect_weight(result, function_list1)
+functions.write_to_file_fun_data(function_list1,function_list2)
 x = functions.list_of_lines('connect_weight.txt')
 final_weight = list(x)
 temp = final_weight[1]
@@ -17,5 +20,14 @@ with open('graf.txt', 'a') as f:
     for el in final_weight:
         f.write(str(el))
         f.write('\n')
+with open('function_weight.txt', 'a') as f:
+    for el in function_list1:
+        f.write(el)
+        f.write('\n')
+with open('function_weight.txt', 'a') as f:
+    for el in function_list1:
+        f.write(str(0))
+        f.write('\n')
+
 
 
