06a79cf7906dc74fe0d7a9e671fb097d9b8f5ef3
wtycjan <56653553+wtycjan@users.noreply.github.com>
2019-12-10 16:02:32 +0100

    Update Header.h

--- Header.h
+++ Header.h
@@ -21,10 +21,11 @@ public:
 	string linia2;
 	fstream dane;
 	int rozmiar_dynamicznej = 0;
-	
+
 
 	string graph = "digraph G {";
 	string dotPath = "C:\\Users\\Tycjan\\Documents\\release\\bin\\dot.exe"; //link do biblioteki
+	string notatnik = "graf_jozin.txt";
 	string tempFile = "temp.dot";
 	string outFile = "out.png";
 };
@@ -43,9 +44,9 @@ public:
 
 		system((dotPath + " " + tempFile + " -Tpng -o " + outFile).c_str());	//tworzenie grafu
 	}
-	void polaczenia(string notatnik) {
+	void polaczenia(string nazwa, string color) {
 
-		
+
 		rozmiar_dynamicznej = 0;	//zerujemy rozmiar dla wielukrotnego wywolania funkcji
 
 		dane.open(notatnik, ios::in);  //txt od Grzesia
@@ -57,32 +58,32 @@ public:
 		}
 		else
 		{
-			ifstream file(notatnik);		//sprawdzanie rozmiaru pliku do stworzenia tablic dynamicznych
+			ifstream file("graf_jozin.txt");		//sprawdzanie rozmiaru pliku do stworzenia tablic dynamicznych
 			while (getline(file, linia2))
 				rozmiar_pliku++;
 
 			string* grafs = new string[rozmiar_pliku];	// stringi potrzebne do rysowania grafu w petli
-			string* linie = new string[rozmiar_pliku]; //tablica do linii
 			string* poloczenia = new string[rozmiar_pliku];//tablica do poloczen
 			string* wagi = new string[rozmiar_pliku]();//tablica do wag
 			for (int i = 0; i < rozmiar_pliku; i++)
 				wagi[i] = "0";			//zerujemy tablice wag
 
 
-
+			int l_polaczen = 0;
 			i = 0;
 
-			while (getline(dane, linia))          //petla zczutyjaca ilosc linie
+			while (getline(dane, linia))
 			{
-				linie[i] = linia;
-				i++;
-				if (linia == "dane") break;
+				if (linia == nazwa) {
+					break;
+				}
 			}
 
 			i = 0;
 			while (getline(dane, linia))          //petla zczytujaca poloczenia miedzy plikami
 			{
 				poloczenia[i] = linia;
+				l_polaczen++;
 				i++;
 				if (linia == "dane") break;
 			}
@@ -109,146 +110,79 @@ public:
 
 				if (wagi[j] == "0")								//sprawdzamy czy musimy tworzyc polaczenie czy wolny wezel
 				{
-					all[j] = poloczenia[i] + "_" + linie[i] + ";\n" + poloczenia[i + 1] + "_" + linie[i + 1];
+					all[j] = quote + poloczenia[i] + quote +  ";\n" +quote + poloczenia[i + 1] + quote;
 					grafs[j] = all[j] + ";\n";
 					i = i + 2;
 				}
 				else {
 
-					all[j] = poloczenia[i] + "_" + linie[i] + znak + poloczenia[i + 1] + "_" + linie[i + 1];
+					all[j] = quote + poloczenia[i] + quote +  znak + quote + poloczenia[i + 1] + quote;
 					grafs[j] = all[j] + "[label = " + wagi[j] + "];\n";
 					i = i + 2;
 				}
 			}
 
 
-			
-			for (igraf = 0; igraf < rozmiar_dynamicznej; igraf++)	// tworzenie polecenia do stworzenia grafu w petli
+
+			for (igraf = 0; igraf < l_polaczen - 1; igraf++)	// tworzenie polecenia do stworzenia grafu w petli
 			{
+				//tworzenie samych wezlow o kolorze
+				graph += "" + quote + poloczenia[igraf] + quote + " [style =filled, color="+color+" ]; \n";
+			}
+			for (igraf = 0; igraf < rozmiar_dynamicznej; igraf++)	
+			{
+				//dodajemy do polecenia
 				graph += grafs[igraf];
 			}
 
+
 			cout << graph;
-			delete[] linie;
 			delete[] poloczenia;
 			delete[] wagi;
+			delete[] grafs;
 		}
 
 		dane.close();
 	}
-	void modulf(string notatnik) {
-		
-		rozmiar_pliku = 0;
-		dane.open(notatnik, ios::in);  //txt od Grzesia
-
-		if (dane.good() == false)
-		{
-			cout << "Nie mozna otworzyc pliku";
-			exit(0);
-		}
-		else
-		{
-			ifstream file(notatnik);		//sprawdzanie rozmiaru pliku do stworzenia tablic dynamicznych
-			while (getline(file, linia2))
-				rozmiar_pliku++;
-
-			string* grafs = new string[rozmiar_pliku];	// stringi potrzebne do rysowania grafu w petli
-			string* linie = new string[rozmiar_pliku]; //tablica do linii
-			string* wagi = new string[rozmiar_pliku]();//tablica do wag
-			for (int i = 0; i < rozmiar_pliku; i++)
-				wagi[i] = "0";			//zerujemy tablice wag
-
-
-
-			i = 0;
-			while (getline(dane, linia))         //petla zczytujaca wagi
-			{
-				wagi[i] = linia;
-
-				if (linia == "dane")
-				{
-					wagi[i] = "0";		//waga od i zeby nie byla rowna "dane"
-					break;
-				}
-				i++;
-			}
-			i = 0;
-			while (getline(dane, linia))          //petla zczutyjaca ilosc linie
-			{
-				linie[i] = linia;
-				i++;
-				rozmiar_dynamicznej++;			//licznik lin jest jednoznaczny z iloscia polaczen
-				if (linia == "dane") break;
-			}
-
-
-
-
-
-			string* all = new string[rozmiar_dynamicznej];
-
-
-			all[0] = quote + linie[0] + quote + znak + linie[1];
-			all[1] = quote + " " + linie[3] + quote + znak + linie[1];		//tworzenie zmiennych do grafu
-			all[2] = linie[2] + znak + quote + linie[0] + quote;
-
-			all[3] = quote + linie[4] + quote + znak + linie[5];	//druga czesc
-			all[4] = linie[6] + znak + quote + linie[4] + quote;
-			all[5] = linie[7] + znak + linie[5];
-
-
-
-
-			graph += all[0] + " [ label = " + wagi[0] + "  ];\n"  //Funkcja rysujaca gra
-				"" + all[1] + "[label = " + wagi[0] + "]; \n"
-				"" + all[2] + "[label = " + wagi[0] + "]; \n"
-				"" + all[3] + "[label = " + wagi[0] + "]; \n"
-				"" + all[4] + "[label = " + wagi[0] + "]; \n"
-				"" + all[5] + "[label = " + wagi[0] + "]; \n";
-				
-			dane.close();
-		}
-	}
-
-	void Functions(string notatnik)
+	void Functions()
 	{
-		polaczenia(notatnik);
+		polaczenia("FUNCTIONS", "lightskyblue1");
 		draw();
 	}
-	void Files(string notatnik)
+	void Files()
 	{
-		polaczenia(notatnik);
+		polaczenia("FILES", "gold1");
 		draw();
 	}
-	void Modules(string notatnik)
+	void Modules()
 	{
-		modulf(notatnik);
+		polaczenia("MODULES","burlywood2");
 		draw();
 	}
-	void PlikiFunkcje(string notatnik1, string notatnik2)
+	void PlikiFunkcje()
 	{
-		polaczenia(notatnik1);
-		polaczenia(notatnik2);
+		polaczenia("FILES", "gold1");
+		polaczenia("FUNCTIONS", "lightskyblue1");
 		draw();
 
 	}
-	void PlikiModuly(string notatnik1, string notatnik2)
+	void PlikiModuly()
 	{
-		polaczenia(notatnik1);
-		modulf(notatnik2);
+		polaczenia("FILES", "gold1");
+		polaczenia("MODULES", "burlywood2");
 		draw();
 	}
-	void FunkcjeModuly(string notatnik1, string notatnik2)
+	void FunkcjeModuly()
 	{
-		polaczenia(notatnik1);
-		modulf(notatnik2);
+		polaczenia("FUNCTIONS", "lightskyblue1");
+		polaczenia("MODULES", "burlywood2");
 		draw();
 	}
-	void FunkcjeModulyPliki(string notatnik1, string notatnik2,string notatnik3)
+	void FunkcjeModulyPliki()
 	{
-		polaczenia(notatnik1);
-		modulf(notatnik2);
-		polaczenia(notatnik3);
+		polaczenia("FUNCTIONS", "lightskyblue1");
+		polaczenia("MODULES", "burlywood2");
+		polaczenia("FILES", "gold1");
 		draw();
 	}
 };
